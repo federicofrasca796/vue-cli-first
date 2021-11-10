ceb561dcc67bf8334b2259bd6ea8845d8b453776
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-10 15:36:51 +0100

    add bonus jumbo

--- new-vue/src/App.vue
+++ new-vue/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <Title msg="La mia prima app con Vue CLI!"/>
+    <Hero/>
   </div>
 </template>
 
 <script>
 import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
--- new-vue/src/components/Hero.vue
+++ new-vue/src/components/Hero.vue
@@ -1,3 +1,17 @@
 <template>
-    
+  <img src="https://picsum.photos/500/300" alt="">
 </template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+img {
+    margin: auto;
+}
+</style>
